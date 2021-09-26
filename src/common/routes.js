6bee9932359e295f83a1479a2f5b00458aca6781
Hassan Abbas <hazzyhassan888@gmail.com>
2021-09-26 11:40:30 +0500

    Minor changes

--- src/common/routes.js
+++ src/common/routes.js
@@ -1,7 +1,7 @@
 import {Platform} from 'react-native'
 export const ENUMS = {
-  baseURL: Platform.OS=="android"?"http://10.0.2.2:5000":"http://localhost:5000",
-  mainApi: "/api/getRequiredName",
-  trendingNames: "/api/getRequiredName/trendingNames",
+  baseURL: "https://www.babynamemeaningz.com",
+  mainApi: "/api/GetNamesWithFiltersAPI.php",
+  trendingNames: "/api/trendingNames.php",
 };
 
--- src/screens/trending/trending.js
+++ src/screens/trending/trending.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 import {
   Text,
   StyleSheet,
@@ -9,41 +9,45 @@ import {
   FlatList,
   Image,
   ActivityIndicator,
-} from "react-native";
-import COLORS from "../../common/colors";
-import images from "../../common/images";
-import BackHeader from "../../components/backHeader";
-import NameListCard from "../../components/cards/nameListCard";
-import GenderOptions from "../../components/genderOptions";
-import ValuePickerModal from "../../components/models/valuePickerModal";
-import SearchBar from "../../components/searchBar";
+} from 'react-native';
+import COLORS from '../../common/colors';
+import images from '../../common/images';
+import BackHeader from '../../components/backHeader';
+import NameListCard from '../../components/cards/nameListCard';
+import GenderOptions from '../../components/genderOptions';
+import ValuePickerModal from '../../components/models/valuePickerModal';
+import SearchBar from '../../components/searchBar';
 import {
   GetOptimalHieght,
   GetOptimalWidth,
   scaledFontSize,
-} from "../../helpers/commonHelpers/helpers";
+} from '../../helpers/commonHelpers/helpers';
 
-const Item = ({ item, index, gotoDetails, addToFav, favorites }) => (
-  <NameListCard
-    item={item}
-    index={index}
-    favorites={favorites}
-    addToFav={addToFav}
-    gotoDetails={() => {
-      gotoDetails();
-    }}
-  />
-);
+const Item = ({item, index, gotoDetails, addToFav, favorites}) => {
+  const fav = favorites?.filter(x => x.id == item?.id);
+  return (
+    <NameListCard
+      fav={fav.length > 0 ? true : false}
+      item={item}
+      index={index}
+      favorites={favorites}
+      addToFav={addToFav}
+      gotoDetails={() => {
+        gotoDetails();
+      }}
+    />
+  );
+};
 export default class Trending extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "",
+      title: '',
     };
   }
   componentDidMount() {
     this.setState({
-      title: "Trending Names",
+      title: 'Trending Names',
     });
   }
 
@@ -52,13 +56,12 @@ export default class Trending extends Component {
       <View
         style={{
           height: GetOptimalHieght(400),
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <Image source={images.missing} style={styles.imageStyle} />
-        <Text>{"Sorry! your searched names does not exist"}</Text>
-        <Text style={styles.textDesc}>{"Try with another name"}</Text>
+        <Text>{'Sorry! your searched names does not exist'}</Text>
+        <Text style={styles.textDesc}>{'Try with another name'}</Text>
       </View>
     );
   };
@@ -72,24 +75,22 @@ export default class Trending extends Component {
       <ImageBackground
         source={images.home}
         style={{
-          width: "100%",
-          height: "100%",
+          width: '100%',
+          height: '100%',
         }}
-        resizeMode="cover"
-      >
+        resizeMode="cover">
         <BackHeader
           title={this.state.title}
           onBackPress={() => {
             this.props.navigation.goBack();
           }}
           gotoHome={() => {
-            this.props.navigation.navigate("Home");
+            this.props.navigation.navigate('Home');
           }}
         />
         {this.props?.namesData?.loading === true ? (
           <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-          >
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <ActivityIndicator size="large" color={COLORS.BLACK} />
           </View>
         ) : (
@@ -98,7 +99,7 @@ export default class Trending extends Component {
             keyExtractor={(item, index) => item + index}
             ListEmptyComponent={this.ListEmptyComponent}
             showsVerticalScrollIndicator={false}
-            renderItem={({ item, index }) => (
+            renderItem={({item, index}) => (
               <Item
                 item={item}
                 index={index}
@@ -109,7 +110,7 @@ export default class Trending extends Component {
                 gotoDetails={() => {
                   this.props.setDetailItem(item);
                   setTimeout(() => {
-                    this.props.navigation.navigate("NameDetails");
+                    this.props.navigation.navigate('NameDetails');
                   }, 200);
                 }}
               />
@@ -127,7 +128,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
   },
   item: {
-    backgroundColor: "#f9c2ff",
+    backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
   },
@@ -137,15 +138,15 @@ const styles = StyleSheet.create({
     width: GetOptimalHieght(25),
     height: GetOptimalHieght(25),
     borderRadius: GetOptimalHieght(20),
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginLeft: GetOptimalWidth(10),
     marginVertical: GetOptimalHieght(10),
   },
   header: {
     fontSize: scaledFontSize(14),
     color: COLORS.WHITE,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   title: {
     fontSize: 24,
@@ -157,7 +158,7 @@ const styles = StyleSheet.create({
   imageStyle: {
     width: GetOptimalHieght(100),
     height: GetOptimalHieght(100),
-    resizeMode: "contain",
+    resizeMode: 'contain',
     marginBottom: GetOptimalHieght(30),
   },
 });
